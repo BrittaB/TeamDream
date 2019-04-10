3adb0469dec80f025ed353674d6719ffac1d3108
jsmithwh <46731513+Jsmithwh@users.noreply.github.com>
2019-04-09 21:09:49 -0500

    another commit

--- assets/java/weather.js
+++ assets/java/weather.js
@@ -1,6 +1,6 @@
-$("#").on("change", function (events) {
+$("#").on("click", function (events) {
 
-  var queryURL = "https://www.metaweather.com/api/location/search/?query=woid";
+  var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name},us&appid57742b48d96448fdbe3c30d3f4a55702";
   //woid is just a placeholder for location pulled from other API"
   $.ajax({
     url: queryURL,
