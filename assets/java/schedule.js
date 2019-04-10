5251ba469f4a24f957a1fd28aed03a338d435fcc
jsmithwh <46731513+Jsmithwh@users.noreply.github.com>
2019-04-09 21:47:55 -0500

    weather api added

--- assets/java/schedule.js
+++ assets/java/schedule.js
@@ -15,28 +15,46 @@ $("#search").on("change", function (event) {
     })
         .then(function (response) {
             console.log(response);
-            var games = response.fullgameschedule;
+            var games = response.fullgameschedule.gameentry;
+            console.log(games);
+            // var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name},us&appid57742b48d96448fdbe3c30d3f4a55702";
+            // for (var i = 0; i < games.length; i++) {
+            //     var date = games.gameentry.date;
+            //     var awayTeam = results.gameentry.awayTeam.Name;
+            //     var homeTeam = results.gameentry.homeTeam.Name;
+            //     var location = results.gameentry.homeTeam.City;
 
             var weatherReqs = games.map(function (game) {
-                return $.ajax(); // get game weather
+                console.log(game);
+                var awayTeam = game.awayTeam.Name;
+                var homeTeam = game.homeTeam.Name;
+                var location = game.homeTeam.City;
+                var date = game.date;
+                var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + ",us&appid=57742b48d96448fdbe3c30d3f4a55702";
+
+                return $.ajax({
+                    url: queryURL,
+                    method: "GET"
+                }); // get game weather
             });
 
             Promise.all(weatherReqs).then(function (weather) {
                 // each index is mapped to the same index as the games array
                 // games[0] -> weather[0]
-                for (var i = 0; i < games.length; i++) {
-                    var date = games.gameentry.date;
-                    var awayTeam = results.gameentry.awayTeam.Name;
-                    var homeTeam = results.gameentry.homeTeam.Name;
-                    var location = results.gameentry.homeTeam.City;
+                // for (var i = 0; i < games.length; i++) {
+                //     var date = games.gameentry.date;
+                //     var awayTeam = results.gameentry.awayTeam.Name;
+                //     var homeTeam = results.gameentry.homeTeam.Name;
+                //     var location = results.gameentry.homeTeam.City;
 
-                    var newRow = $("<tr>").append(
-                        $("<td>").text(awayTeam),
-                        $("<td>").text(homeTeam),
-                        $("<td>").text(location),
-                        $("#tableID > tbody").append(newRow)
-                    );
-                }
+                //     var newRow = $("<tr>").append(
+                //         $("<td>").text(awayTeam),
+                //         $("<td>").text(homeTeam),
+                //         $("<td>").text(location),
+                //         $("#tableID > tbody").append(newRow)
+                // );
+                console.log(weather);
+
             });
         })
 }
---
+++ assets/java/weather.js
@@ -0,0 +1,20 @@
+$("#").on("click", function (events) {
+
+  var queryURL = "api.openweathermap.org/data/2.5/forecast?q={city name},us&appid57742b48d96448fdbe3c30d3f4a55702";
+ 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+    .then(function (gimme) {
+      console.log(gimme);
+      var yesGive=gimme.allWeather
+  for (var i=0; i<yesGive.length; i++){
+
+  }
+
+}
+)
+
+});
